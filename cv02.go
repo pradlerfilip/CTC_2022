65540e0ab4e3afa1acdfb005acf6fdd8a56c20a5
pradlerfilip <62916869+pradlerfilip@users.noreply.github.com>
2022-04-05 23:18:51 +0200

    Update cv02.go

--- cv02.go
+++ cv02.go
@@ -22,17 +22,18 @@ type queue struct {
 	payedCars   []*car
 }
 
-func newQueue() *queue {
-	q := queue{waitingCars: []*car{newCar(gas), newCar(gas), newCar(gas)}}
-	return &q
-}
+//func newQueue() *queue {
+//q := queue{waitingCars: []*car{newCar(gas), newCar(gas), newCar(gas)}}
+//return &q
+//}
 
 type car struct {
+	id       int
 	needFuel Fuel
 }
 
-func newCar(fuel Fuel) *car {
-	c := car{needFuel: fuel}
+func newCar(fuel Fuel, id int) *car {
+	c := car{needFuel: fuel, id: id}
 	return &c
 }
 
@@ -46,10 +47,12 @@ func newPump(fuel Fuel) *pump {
 }
 
 type cashRegister struct {
+	id   int
+	used bool
 }
 
-func newCashRegister() *cashRegister {
-	cR := cashRegister{}
+func newCashRegister(id int) *cashRegister {
+	cR := cashRegister{used: false, id: id}
 	return &cR
 }
 
@@ -80,37 +83,56 @@ func newStation() *station {
 	return &s
 }
 
-func usePump(typeOfFuel Fuel, queue *[100]*car, wg *sync.WaitGroup) {
+func usePump(typeOfFuel Fuel, queue *[100]*car, cash_registers *[2]*cashRegister, wg *sync.WaitGroup, channel chan int) {
+	defer wg.Done()
 	fmt.Println(typeOfFuel + " ready")
 	var wg2 sync.WaitGroup
+	var wg3 sync.WaitGroup
 	for i := 0; i < len(queue); i++ {
-		wg2.Add(1)
-		go func() {
-			time.Sleep(2 * time.Second)
-			fmt.Println(queue[i].needFuel + " filled")
-			wg2.Done()
-		}()
-
+		if queue[i].needFuel == typeOfFuel {
+			wg2.Add(1)
+			go func() {
+				time.Sleep(1 * time.Second)
+				fmt.Print(queue[i].id)
+				fmt.Println("of type: " + queue[i].needFuel + " is filled")
+				wg3.Add(1)
+				for j := 0; j < len(cash_registers); j++ {
+					if cash_registers[j].used == false {
+						cash_registers[j].used = true
+						time.Sleep(2 * time.Second)
+						fmt.Println("of type: " + queue[i].needFuel + " payed")
+						cash_registers[j].used = false
+						wg3.Done()
+					}
+					wg3.Wait()
+				}
+				wg2.Done()
+			}()
+			wg2.Wait()
+		}
 	}
-	wg2.Wait()
-	wg.Done()
 }
 
 func main() {
 	var wg sync.WaitGroup
 	var queue [100]*car
+	var cash_registers [2]*cashRegister
+	channel := make(chan int, 2)
 	gasTypes := []string{"gas", "diesel", "lpg", "electric"}
 	for i := 0; i < len(queue); i++ {
-		queue[i] = newCar(Fuel(gasTypes[rand.Intn(len(gasTypes)-0)+0]))
+		queue[i] = newCar(Fuel(gasTypes[rand.Intn(len(gasTypes)-0)+0]), i)
+	}
+	for i := 0; i < len(cash_registers); i++ {
+		cash_registers[i] = newCashRegister(i)
 	}
 	wg.Add(1)
-	go usePump("gas", &queue, &wg)
+	go usePump("gas", &queue, &cash_registers, &wg, channel)
 	wg.Add(1)
-	go usePump("diesel", &queue, &wg)
+	go usePump("diesel", &queue, &cash_registers, &wg, channel)
 	wg.Add(1)
-	go usePump("lpg", &queue, &wg)
+	go usePump("lpg", &queue, &cash_registers, &wg, channel)
 	wg.Add(1)
-	go usePump("electric", &queue, &wg)
+	go usePump("electric", &queue, &cash_registers, &wg, channel)
 	wg.Wait()
 	fmt.Println("Done")
 }
