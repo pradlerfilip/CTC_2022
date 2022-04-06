853d5b05212c5507ee60e786adcd3a34e98b8d2f
pradlerfilip <62916869+pradlerfilip@users.noreply.github.com>
2022-04-06 16:13:44 +0200

    Update cv02.go

--- cv02.go
+++ cv02.go
@@ -15,17 +15,22 @@ const (
 	electric = "electric"
 )
 
-func fillCar(n_of_cars int, fuel Fuel, capacity chan int, car_queue *sync.WaitGroup) {
+func fillCar(n_of_cars int, fuel Fuel, pump_capacity chan int, cash_registers_capacity chan int, car_queue *sync.WaitGroup) {
 	var wg2 sync.WaitGroup
 	for i := 1; i <= n_of_cars; i++ {
-		capacity <- 1
+		pump_capacity <- 1
 		wg2.Add(1)
 		go func() {
 			fmt.Println("Fuelling... " + fuel)
 			time.Sleep(time.Second * 1)
 			fmt.Println("Fuelling: " + fuel + "  completed")
-			<-capacity
+			cash_registers_capacity <- 1
+			fmt.Println("Car with: " + fuel + "  is paying...")
+			time.Sleep(time.Millisecond * 500)
+			fmt.Println("Car with: " + fuel + "  payed")
 			wg2.Done()
+			<-cash_registers_capacity
+			<-pump_capacity
 		}()
 	}
 	wg2.Wait()
@@ -35,21 +40,22 @@ func fillCar(n_of_cars int, fuel Fuel, capacity chan int, car_queue *sync.WaitGr
 func main() {
 
 	var car_queue sync.WaitGroup
-	capacity_gas := make(chan int, 4)
-	capacity_diesel := make(chan int, 4)
-	capacity_lpg := make(chan int, 4)
-	capacity_electric := make(chan int, 2)
+	cash_registers_capacity := make(chan int, 2)
+	pump_capacity_gas := make(chan int, 4)
+	pump_capacity_diesel := make(chan int, 4)
+	pump_capacity_lpg := make(chan int, 4)
+	pump_capacity_electric := make(chan int, 2)
 	var n_of_gas int = 5
 	var n_of_diesel int = 5
 	var n_of_lpg int = 5
 	var n_of_electric int = 5
 	car_queue.Add(1)
-	go fillCar(n_of_gas, "gas", capacity_gas, &car_queue)
+	go fillCar(n_of_gas, "gas", pump_capacity_gas, cash_registers_capacity, &car_queue)
 	car_queue.Add(1)
-	go fillCar(n_of_diesel, "diesel", capacity_diesel, &car_queue)
+	go fillCar(n_of_diesel, "diesel", pump_capacity_diesel, cash_registers_capacity, &car_queue)
 	car_queue.Add(1)
-	go fillCar(n_of_lpg, "lpg", capacity_lpg, &car_queue)
+	go fillCar(n_of_lpg, "lpg", pump_capacity_lpg, cash_registers_capacity, &car_queue)
 	car_queue.Add(1)
-	go fillCar(n_of_electric, "electric", capacity_electric, &car_queue)
+	go fillCar(n_of_electric, "electric", pump_capacity_electric, cash_registers_capacity, &car_queue)
 	car_queue.Wait()
 }
